7f391b57b52f976cd0a3e9ef06291b6d9c05e101
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2022-12-16 17:32:53 +0000

    chore: Set up logging in __init__

--- src/scandi_reddit/__init__.py
+++ src/scandi_reddit/__init__.py
@@ -2,7 +2,17 @@
 .. include:: ../../README.md
 """
 
+import logging
+
 import pkg_resources
 
 # Fetches the version of the package as defined in pyproject.toml
 __version__ = pkg_resources.get_distribution("scandi_reddit").version
+
+
+# Set up logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] <%(name)s> %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
